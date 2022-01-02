d3436b993d8843812d1e1fd4ca35e2ff5ecb50e4
aki09 <akshatbhatia05@gmail.com>
2022-01-02 20:27:41 +0530

    here

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
+const request = require('request');
 
 require('dotenv').config();
 
@@ -12,6 +13,15 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/home', function (req, res) {
+    request('http://127.0.0.1:5000/', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the data received
+        res.send(body); //Display the response on the website
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
